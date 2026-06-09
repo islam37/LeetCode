10be3c8071616e5e298c059aea280e2accced84f
islam37 <islamuddin3725@gmail.com>
2026-06-09 17:52:01 +0600

    valid palindrome

---
+++ test.js
@@ -0,0 +1,15 @@
+function isPalindrome(str) {{
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = cleanedStr.length - 1;
+    while (left < right) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}}
+
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
